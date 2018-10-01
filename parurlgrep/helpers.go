5ed865981190a30ffb7e61e304deeb3a6c90fa89
xpinguin <xpinguin@users.noreply.github.com>
2018-10-01 22:25:31 +0300

    NewURLReader: support for file paths and file URIs

--- parurlgrep/helpers.go
+++ parurlgrep/helpers.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"bufio"
+	//"fmt"
 	"io"
 	"log"
 	"net/http"
+	"net/url"
+	"os"
+	"path/filepath"
 )
 
 ///////////
@@ -27,11 +31,50 @@ type URLReader struct {
 	io.RuneReader
 }
 
-func NewURLReader(url URL) *URLReader {
-	r, err := http.Get(url)
-	if r == nil {
-		log.Printf("ERROR: http.Get(%s): %v", url, err)
-		return nil
+func NewURLReader(uraw URL) (urdr *URLReader) {
+	path := uraw
+	scheme := ""
+	// --
+	u, err := url.Parse(uraw)
+	if u != nil {
+		// HACK: for instance, Win32 drive letter s parsed as a scheme
+		if len(u.Scheme) > 1 /* && len(u.Opaque) == 0*/ {
+			path = u.Path
+			scheme = u.Scheme
+		}
+	} else {
+		log.Printf("{ERR} failed to parse '%s' (net/url): %v", uraw, err, u)
 	}
-	return &URLReader{url, r.Body, bufio.NewReader(r.Body)}
+	// --
+	switch scheme {
+	/// as network URI
+	case "http", "https":
+		ustr := u.String()
+		r, err := http.Get(ustr)
+		if r == nil {
+			log.Printf("ERROR: http.Get(%s): %v", ustr, err)
+			return
+		}
+		urdr = &URLReader{ustr, r.Body, bufio.NewReader(r.Body)}
+		log.Printf(">> Parsed as HTTP URI: %v", u)
+	/// as Path
+	case "", "file":
+		p, err := filepath.Abs(path)
+		if err != nil {
+			log.Printf("{ERR} failed to parse '%v' as path (%p)", err, p)
+			return
+		}
+		f, err := os.Open(p)
+		if f == nil {
+			log.Printf("{ERR} unable to open file: %v", err)
+			return
+		}
+		urdr = &URLReader{p, f, bufio.NewReader(f)}
+		log.Printf(">> Parsed as FilePath: %v", u)
+	/// unknown scheme
+	default:
+		log.Printf("{ERR} unknown URL(%v) scheme: '%v' ", u, scheme)
+	}
+	// --
+	return
 }
