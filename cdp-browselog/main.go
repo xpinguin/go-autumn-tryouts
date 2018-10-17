258e206b02bc7de19025310fbe2b9183b6cd30f2
xpinguin <xpinguin@users.noreply.github.com>
2018-10-17 19:45:47 +0300

    cdp-browselog: print content bounding box (boxmodel.Content) for each DOM node in the current document (tab)

--- cdp-browselog/main.go
+++ cdp-browselog/main.go
@@ -2,12 +2,22 @@ package main
 
 import (
 	Ctx "context"
+	"fmt"
 	"log"
 
-	//"github.com/chromedp/cdproto/cdp"
+	"github.com/chromedp/cdproto/cdp"
+	"github.com/chromedp/cdproto/dom"
+
+	//"github.com/chromedp/cdproto/domsnapshot"
 	"github.com/chromedp/chromedp"
 	cdpclient "github.com/chromedp/chromedp/client"
 )
+
+/*func ByQueryAll(s *Selector) {
+	ByFunc(func(ctxt context.Context, h *TargetHandler, n *cdp.Node) ([]cdp.NodeID, error) {
+		return dom.QuerySelectorAll(n.NodeID, s.selAsString()).Do(ctxt, h)
+	})(s)
+}*/
 
 func main() {
 	//
@@ -15,7 +25,7 @@ func main() {
 	ctx := Ctx.Background()
 
 	//
-	_cdev_client := cdpclient.New(cdpclient.URL("http://localhost:9222/json"))
+	_cdev_client := cdpclient.New(cdpclient.URL("http://localhost:9851/json"))
 	cdev, err := chromedp.New(ctx, chromedp.WithClient(ctx, _cdev_client))
 	if cdev == nil {
 		log.Fatalf("!cdev; err = %v", err)
@@ -25,10 +35,10 @@ func main() {
 
 	// list open tabs (page-targets)
 	// --
-	ListPageTargets := func(ctx Ctx.Context) []cdpclient.Target {
+	/*ListPageTargets := func(ctx Ctx.Context) []cdpclient.Target {
 		ts, _ := _cdev_client.ListPageTargets(ctx)
 		return ts
-	}
+	}*/
 
 	// -- revert back current tab
 	/*cdev.Lock()
@@ -45,7 +55,7 @@ func main() {
 	defer cdev.SetHandlerByID(active_page.GetID())*/
 
 	// -- iterate through all
-	for i, page := range ListPageTargets(ctx) {
+	/*for i, page := range ListPageTargets(ctx) {
 		if err := _cdev_client.ActivateTarget(ctx, page); err != nil {
 			log.Fatalf("ActivateTarget(%v): err = %v", page, err)
 		}
@@ -57,7 +67,56 @@ func main() {
 		})
 		log.Printf("[%d] %s {target-id:%v}", i, page_url, page.GetID())
 
+	}*/
+
+	// box model (for the current target?)
+	var page_url string
+	//var boxmodel *dom.BoxModel
+
+	err = cdev.Run(ctx, chromedp.Tasks{
+		cdev.SetTarget(0),
+		chromedp.Evaluate("document.location.toString()", &page_url),
+		//chromedp.WaitVisible("/*", chromedp.ByQueryAll),
+		//chromedp.Dimensions("div", &boxmodel, chromedp.BySearch),
+		chromedp.QueryAfter(
+			"*",
+			func(ctxt Ctx.Context, h *chromedp.TargetHandler, nodes ...*cdp.Node) error {
+				if len(nodes) < 1 {
+					return fmt.Errorf("selector did not return any nodes")
+				}
+				for _, n := range nodes {
+					nbox, err := dom.GetBoxModel().WithNodeID(n.NodeID).Do(ctxt, h)
+					if err != nil {
+						log.Printf("{ERR} <%s>: %v", n.NodeName, err)
+						continue
+					}
+					// --
+					log.Printf("<%s>: %v", n.NodeName, nbox.Content)
+				}
+				return nil
+			},
+			chromedp.BySearch,
+		),
+	})
+	if err != nil {
+		log.Fatalf("{ERR} Run: err = %v", err)
 	}
 
-	// further actions
+	/*log.Printf("URL: %s ;; BOXMODEL: %p", page_url, boxmodel)
+	//log.Printf("BOXMODEL OUTER SIZE: %d", len(boxmodel))
+
+	if boxmodel == nil {
+		panic("!boxmodel")
+	}
+	fmt.Printf("\n\n----------------------------\n")
+	fmt.Println(*boxmodel)
+	fmt.Println(boxmodel.ShapeOutside)*/
+
+	// DOM snapshot
+	/*cstyles := make([]string)
+	domcapture_cmd := domsnapshot.CaptureSnapshot(cstyles)
+	// --
+	if err := cdev.Run(ctx, domcapture_cmd); err != nil {
+		log.Fatalf("RUN: domsnapshot.CaptureSnapshot(): err = %v", err)
+	}*/
 }
