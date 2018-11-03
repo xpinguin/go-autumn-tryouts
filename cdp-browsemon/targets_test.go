6639e7a5a2104be1ee3c3db815f82e7cda9135eb
xpinguin <xpinguin@users.noreply.github.com>
2018-11-03 13:40:20 +0300

    test DOM availability (GetRoot) for the tab

--- cdp-browsemon/targets_test.go
+++ cdp-browsemon/targets_test.go
@@ -3,11 +3,39 @@ package browsemon
 import (
 	"log"
 	T "testing"
+	"time"
 )
 
+func dialBrowser() BrowserContext {
+	return DialBrowser("localhost", 9851)
+}
+
+func sleep(seconds int) {
+	time.Sleep(time.Second * time.Duration(seconds))
+}
+
 func TestListTabs(t *T.T) {
-	b := DialBrowser("localhost", 9851)
-	for th := range b.PageHandlersStream() {
-		log.Printf("<PageHandler>: %v", th)
-	}
+	b := dialBrowser()
+	go func() {
+		for th := range b.PageHandlersStream() {
+			log.Printf("<PageHandler>: %v", th)
+		}
+	}()
+	sleep(5)
+}
+
+func TestTabRoot(t *T.T) {
+	b := dialBrowser()
+	go func() {
+		for th := range b.PageHandlersStream() {
+			root, err := th.GetRoot(b.ctx)
+			if err != nil {
+				t.Fatal("{ERR} GetRoot(ctx): ", err)
+			}
+			log.Printf("root DOM Node: %v", root)
+			log.Printf("root children: %v", root.Children)
+			break // one tab is enough
+		}
+	}()
+	sleep(5)
 }
